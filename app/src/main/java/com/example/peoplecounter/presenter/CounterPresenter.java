b0df930b3acefb1a13aee9ea51682a58448bdc47
Jperzval <perezvalle.jes@gmail.com>
2020-09-24 22:12:20 -0400

    created counter presenter and implemented presenter interface

---
+++ app/src/main/java/com/example/peoplecounter/presenter/CounterPresenter.java
@@ -0,0 +1,31 @@
+package com.example.peoplecounter.presenter;
+
+import com.example.peoplecounter.Contract;
+
+public class CounterPresenter implements Contract.Presenter {
+
+    @Override
+    public void onCounterStart() {
+
+    }
+
+    @Override
+    public void increasePeopleCount() {
+
+    }
+
+    @Override
+    public void decreasePeopleCount() {
+
+    }
+
+    @Override
+    public void resetCount() {
+
+    }
+
+    @Override
+    public void getSavedCount() {
+
+    }
+}
