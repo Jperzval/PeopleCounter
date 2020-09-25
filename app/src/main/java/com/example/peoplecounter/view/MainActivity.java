835b908dec0710ba754e566fff6af1000038570a
Jperzval <perezvalle.jes@gmail.com>
2020-09-24 22:13:03 -0400

    implmented view interface in MainActivity

--- app/src/main/java/com/example/peoplecounter/view/MainActivity.java
+++ app/src/main/java/com/example/peoplecounter/view/MainActivity.java
@@ -4,13 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.peoplecounter.Contract;
 import com.example.peoplecounter.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Contract.View {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void displayTotalCount(int total) {
+        
+    }
+
+    @Override
+    public void displayCurrentCount(int currentCount) {
+
+    }
+
+    @Override
+    public void changeTextColorToRed() {
+
+    }
+
+    @Override
+    public void changeTextColorToPrimaryDark() {
+
+    }
+
+    @Override
+    public void hideMinusButton() {
+
+    }
+
+    @Override
+    public void showMinusButton() {
+
     }
 }
